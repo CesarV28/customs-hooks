d924d14e8640fdd2665d5a2447dceeb9f8c39050
Cesar Vargas <CesarVargas27@outlook.com>
2022-07-02 18:51:35 -0500

    Primer commit

---
+++ hooks/useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('TODOS_RR01')) || [];
+}
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, [], init);
+
+    useEffect(() => {
+      localStorage.setItem('TODOS_RR01', JSON.stringify(todos));
+    }, [todos])
+    
+
+    const handleNewTodo = ( todo ) => {
+        
+        const action = {
+            type: '[TODO]: Add',
+            payload: todo,
+        }
+
+        dispatch( action );
+    }
+
+    const handleRemoveTodo = ( id ) => {
+        dispatch({
+            type: '[TODO]: Remove',
+            payload: id,
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO]: Complete',
+            payload: id,
+        });
+    }
+
+    const todosCount = () => todos.length
+
+
+    const pendingTodosCount = () => todos.filter(todo => !todo.done).length
+    
+
+    return {
+        todos,
+        handleNewTodo, 
+        handleRemoveTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount,
+    }
+}
